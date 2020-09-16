5308abd9a67dc3e26ba1f033003a57fea2eee873
NetoRocha97 <jose.rocha@dcx.ufpb.br>
2020-09-16 13:39:27 +0000

    Returns method formatted as requested

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -2,21 +2,32 @@ package com.example.project;
 
 public class CupomFiscal {
 
-	public static String NOME_LOJA = "Arcos Dourados Com. de Alimentos LTDA";
-	public static String LOGRADOURO = "Av. Projetada Leste";
-	public static int NUMERO = 500;
-	public static String COMPLEMENTO = "EUC F32/33/34";
-	public static String BAIRRO = "Br. Sta Genebra";
-	public static String MUNICIPIO = "Campinas";
-	public static String ESTADO = "SP";
-	public static String CEP = "13080-395";
-	public static String TELEFONE = "(19) 3756-7408";
-	public static String OBSERVACAO = "Loja 1317 (PDP)";
-	public static String CNPJ = "42.591.651/0797-34";
-	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
+	static String nome_loja = "Arcos Dourados Com. de Alimentos LTDA";
+	static String logradouro = "Av. Projetada Leste";
+	static int numero = 500;
+	static String complemento = "EUC F32/33/34";
+	static String bairro = "Br. Sta Genebra";
+	static String municipio = "Campinas";
+	static String estado = "SP";
+	static String cep = "13080-395";
+	static String telefone = "(19) 3756-7408";
+	static String observacao = "Loja 1317 (PDP)";
+  static String cnpj = "42.591.651/0797-34";
+	static String inscricao_estadual = "244.898.500.113";
 
-	public static void main(final String[] args) {
-		//Implemente aqui
+  private static void imprime_dados_loja() {
+    System.out.printf("%s\n", nome_loja);
+    System.out.printf("%s, %d %s\n", logradouro, numero, complemento);
+    System.out.printf("%s - %s - %s\n ", bairro, municipio, estado);
+    System.out.printf("CEP:%s Tel %s\n", cep, telefone);
+    System.out.printf(observacao);
+    System.out.printf("CNPJ: %s\n", cnpj);
+    System.out.printf("IE: %s\n", inscricao_estadual);
+
+  }
+
+	public static void main(String[] args) {
+		imprime_dados_loja();
 	}
 
 }
